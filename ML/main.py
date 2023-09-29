78346b6b6fdde5a2a99c57cc81012c79c0c1c7a9
hadar <hadar.urecsys@gmail.com>
2023-09-30 01:06:35 +0300

    improved ML/main.py with a hyperparameter tuning nested loop

--- ML/main.py
+++ ML/main.py
@@ -1,52 +1,76 @@
-from ML.Zoo import rnn, seq2seq
+from ML.Zoo import seq2seq
 from ML.Core.trainer import Trainer
-from ML.Utilities import utils
 import torch
 import torch.nn as nn
 from torchinfo import summary
 
-
-def init_trainer(
-        forces: torch.Tensor,
-        kinematics: torch.Tensor,
-        train_percent=0.85,
-        val_percent=0.1,
-        feature_win=120,
-        target_win=1,
-        intersect=0,
-        batch_size=64,
-        n_epochs=20,
-        seed=3407,
-        criterion=nn.L1Loss(),
-        patience=10,
-        patience_tolerance=0.005,
-        features_norm='zscore',
-        features_global_norm=True,
-        targets_norm='identity',
-        targets_global_norm=True
-) -> Trainer:
-    input_size, output_size = forces.shape[-1], kinematics.shape[-1]
-    input_dim = (batch_size, feature_win, input_size)
-    model = seq2seq.Seq2Seq(input_dim, target_win, 5, 5, 1, True, 5, 5, output_size)
-    optimizer = torch.optim.Adam(model.parameters())
-    print(summary(model, input_size=(batch_size, feature_win, input_size)))
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    trainer = Trainer(forces, kinematics, train_percent, val_percent, feature_win, target_win, intersect, batch_size,
-                      model, model.name, optimizer, criterion, device, patience, patience_tolerance, n_epochs, seed,
-                      features_norm, targets_norm, features_global_norm, targets_global_norm)
-    return trainer
-
-
 if __name__ == '__main__':
     exp_time = '22_09_2023'
+    train_percent = 0.85
+    val_percent = 0.1
+    feature_win = 120
+    target_win = 1
+    intersect = 0
+    batch_size = 256
+    n_epochs = 20
+    seed = 3407
+    criterion = nn.L1Loss()
+    patience = 10
+    patience_tolerance = 0.005
+    features_norm = 'zscore'
+    features_global_norm = True
+    targets_norm = 'identity'
+    targets_global_norm = True
 
-    use_hard_drive = False
-    parent_dirname = "E:\\Hadar\\experiments" if use_hard_drive else '..\\Results'
+    forces = torch.load('/home/hadar/Downloads/forces_small.pt')
+    kinematics = torch.load('/home/hadar/Downloads/kinematics_small.pt')
 
-    forces = torch.load(f"{parent_dirname}\\{exp_time}\\forces.pt")
-    kinematics = torch.load(f"{parent_dirname}\\{exp_time}\\kinematics.pt")
+    input_size, output_size = forces.shape[-1], kinematics.shape[-1]
+    input_dim = (batch_size, feature_win, input_size)
 
-    trainer = init_trainer(forces=forces, kinematics=kinematics)
-    trainer.fit()
+    for emb_size in [8, 16, 32, 64, 128, 256]:
+        for hid_size in [8, 16, 32, 64, 128, 256]:
+            for fwin in [64, 128, 256, 512]:
+                for nlayers in [1, 2, 3]:
+                    exp_name = f""
+                    model = seq2seq.Seq2Seq(
+                        input_dim=input_dim,
+                        target_lag=target_win,
+                        enc_embedding_size=emb_size,
+                        enc_hidden_size=hid_size,
+                        enc_num_layers=nlayers,
+                        enc_bidirectional=True,
+                        dec_embedding_size=emb_size,
+                        dec_hidden_size=hid_size,
+                        dec_output_size=output_size
+                    )
+                    print(summary(model, input_size=(batch_size, feature_win, input_size)))
+                    optimizer = torch.optim.Adam(model.parameters())
+
+                    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+                    trainer = Trainer(
+                        features=forces,
+                        targets=kinematics,
+                        train_percent=train_percent,
+                        val_percent=val_percent,
+                        feature_win=feature_win,
+                        target_win=target_win,
+                        intersect=intersect,
+                        batch_size=batch_size,
+                        model=model,
+                        exp_name=exp_name,
+                        optimizer=optimizer,
+                        criterion=criterion,
+                        device=device,
+                        patience=patience,
+                        patience_tolerance=patience_tolerance,
+                        n_epochs=n_epochs,
+                        seed=seed,
+                        features_norm_method=features_norm,
+                        features_global_normalizer=features_global_norm,
+                        targets_norm_method=targets_norm,
+                        targets_global_normalizer=targets_global_norm
+                    )
+                    trainer.fit()
+
     trainer.predict()
