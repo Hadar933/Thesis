7b06fd724acafa78846fca2e9703515f0b311594
hadar <hadar.urecsys@gmail.com>
2023-10-10 16:20:46 +0300

    [mlp] added __str__

--- ML/Zoo/mlp.py
+++ ML/Zoo/mlp.py
@@ -5,22 +5,28 @@ import torch.nn as nn
 
 
 class MLP(nn.Module):
-    def __init__(self, input_dim: int, history_size: int, output_dim: int, hidden_dims_list: List[int]):
+    def __init__(self, input_size: int, history_size: int, output_size: int, hidden_dims_list: List[int]):
         """
         an MLP implementation that performs (B,H,F) -> (B,H*F) -> MLP -> (B,Out_dim)
-        :param input_dim: the number of features in the input, required to calculate the MLP input dim
+        :param input_size: the number of features in the input, required to calculate the MLP input dim
         :param history_size: feature window, essentially, required to calculate the MLP input dim
-        :param output_dim: output dim for multi-target prediction
+        :param output_size: output dim for multi-target prediction
         :param hidden_dims_list: the dimensions of the hidden layers only (without input and output)
         """
         super(MLP, self).__init__()
+        self.input_size = input_size
+        self.history_size = history_size
+        self.output_size = output_size
         self.flatten = nn.Flatten()
-        self.input_layer = nn.Linear(input_dim * history_size, hidden_dims_list[0])
+        self.input_layer = nn.Linear(input_size * history_size, hidden_dims_list[0])
         self.hidden_layers = nn.ModuleList()
         for i in range(len(hidden_dims_list) - 1):
             self.hidden_layers.append(nn.Linear(hidden_dims_list[i], hidden_dims_list[i + 1]))
             self.hidden_layers.append(nn.ReLU())
-        self.output_layer = nn.Linear(hidden_dims_list[-1], output_dim)
+        self.output_layer = nn.Linear(hidden_dims_list[-1], output_size)
+
+    def __str__(self):
+        return f"MLP[{self.input_size * self.history_size},{self.output_size}]"
 
     def forward(self, x):
         x = self.flatten(x)
@@ -28,4 +34,4 @@ class MLP(nn.Module):
         for layer in self.hidden_layers:
             x = layer(x)
         x = self.output_layer(x)
-        return x
+        return x.unsqueeze(1)
