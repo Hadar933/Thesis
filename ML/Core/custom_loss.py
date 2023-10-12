ab4d55966884d656968f9816d9619bbf447500e1
hadar <hadar933@gmail.com>
2023-10-12 12:32:49 +0300

    [custom_loss.py] changed the format such that each loss is a torch loss (inherits from nn.Module)

--- ML/Core/custom_loss.py
+++ ML/Core/custom_loss.py
@@ -1,21 +1,32 @@
 import torch
+import torch.nn as nn
 
 
-class Loss:
+class TotalVariationLossL1(nn.Module):
+	def __init__(self):
+		super(TotalVariationLossL1, self).__init__()
+
+	def forward(self, signals):
+		diff_signal = torch.sum(torch.abs(signals[:, :, 1:] - signals[:, :, :-1]))
+		return diff_signal
+
+
+class TotalVariationLossMSE(nn.Module):
+	def __init__(self):
+		super(TotalVariationLossMSE, self).__init__()
+
+	def forward(self, signals):
+		diff_signal = torch.sum((signals[:, :, 1:] - signals[:, :, :-1]) ** 2)
+		return diff_signal
+
+
+class LossFactory:
 	@staticmethod
-	def total_variation_l1(signals):
-		return torch.sum(torch.abs(signals[:, :, 1:] - signals[:, :, :-1]))
-
-	@staticmethod
-	def total_variation_mse(signals):
-		return torch.sum((signals[:, :, 1:] - signals[:, :, :-1]) ** 2)
-
-	@staticmethod
-	def get_loss_function(loss_name):
+	def get_loss(loss_name):
 		match loss_name.lower():
 			case 'total_variation_mae' | 'total_variation_l1' | 'tv_l1' | 'tv_mae':
-				return Loss.total_variation_l1
+				return TotalVariationLossL1()
 			case 'total_variation_mse' | 'total_variation_l2' | 'tv_l2' | 'tv_mse':
-				return Loss.total_variation_mse
+				return TotalVariationLossMSE()
 			case _:  # tries loading default name from torch
 				getattr(torch.nn, loss_name)()
