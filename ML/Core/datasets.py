081c8f397c82de26c8ba10ca2c1df6761088ad62
hadar <hadar933@gmail.com>
2023-10-24 01:08:48 +0300

    [datasets] working on a new dataset that allows for variable length experiments

--- ML/Core/datasets.py
+++ ML/Core/datasets.py
@@ -1,9 +1,11 @@
-from typing import Tuple
+import random
+
 from torch.utils.data import Dataset
 import torch
+from tqdm import tqdm
 
 
-class MultiTimeSeries(Dataset):
+class FixedLenMultiTimeSeries(Dataset):
 	def __init__(
 			self,
 			features: torch.Tensor,
@@ -46,11 +48,78 @@ class MultiTimeSeries(Dataset):
 	def __getitem__(
 			self,
 			idx: int
-	) -> Tuple[torch.Tensor, torch.Tensor]:
-		# TODO: maybe add random start index.
+	) -> tuple[torch.Tensor, torch.Tensor]:
 		ds_idx = idx // self.n_windows_per_ds
 		win_idx = idx % self.n_windows_per_ds
 		features_window = self.normalized_features[ds_idx, win_idx: win_idx + self.feature_win]
-		target_window = self.normalized_targets[ds_idx, win_idx + self.feature_win - self.feature_target_intersect:
-														win_idx + self.feature_win - self.feature_target_intersect + self.target_win]
+		target_window = self.normalized_targets[
+						ds_idx,
+						win_idx + self.feature_win - self.feature_target_intersect:
+						win_idx + self.feature_win - self.feature_target_intersect + self.target_win
+						]
 		return features_window, target_window
+
+
+class VariableLenMultiTimeSeries(Dataset):
+	def __init__(
+			self,
+			features: list[torch.Tensor],
+			targets: list[torch.Tensor],
+			feature_win: int,
+			target_win: int,
+			feature_target_intersection: int
+	):
+		self.feature_win: int = feature_win
+		self.target_win: int = target_win
+		self.feature_target_intersect: int = feature_target_intersection
+		self.normalized_features: list[torch.Tensor] = features
+		self.normalized_targets: list[torch.Tensor] = targets
+
+		# Pre-calculate all rolling windows and store them in a list.
+		# Each element in the list is a tuple containing a feature window and a target window.
+		self.windows = self.calculate_windows()
+
+	def calculate_windows(self) -> list[tuple[torch.Tensor, torch.Tensor]]:
+		windows = []
+		for ds_idx, (feature_dataset, target_dataset) in tqdm(enumerate(
+				zip(self.normalized_features, self.normalized_targets)), total=len(self.normalized_features)):
+			n_samples = feature_dataset.shape[0]
+			n_windows = n_samples - self.feature_win + self.feature_target_intersect - self.target_win + 1
+			for win_idx in range(n_windows):
+				feature_window = feature_dataset[win_idx: win_idx + self.feature_win]
+				target_window = target_dataset[
+								win_idx + self.feature_win - self.feature_target_intersect:
+								win_idx + self.feature_win - self.feature_target_intersect + self.target_win
+								]
+				windows.append((feature_window, target_window))
+		return windows
+
+	def __len__(self) -> int:
+		return len(self.windows)
+
+	def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
+		return self.windows[idx]
+
+
+if __name__ == '__main__':
+	n_datasets, n_features, n_targets = 100, 4, 3
+	fixed_history_size = 3000
+	feature_win, target_win, intersect = 120, 1, 0
+	flmts = FixedLenMultiTimeSeries(
+		features=torch.rand(n_datasets, fixed_history_size, n_features),
+		targets=torch.rand(n_datasets, fixed_history_size, n_targets),
+		feature_win=feature_win,
+		target_win=target_win,
+		feature_target_intersection=intersect
+	)
+	x_flmts, y_flmts = next(iter(flmts))
+
+	histories = [random.randint(fixed_history_size - 100, fixed_history_size + 100) for _ in range(n_datasets)]
+	vlmts = VariableLenMultiTimeSeries(
+		features=[torch.rand(history_size, n_features) for history_size in histories],
+		targets=[torch.rand(history_size, n_targets) for history_size in histories],
+		feature_win=feature_win,
+		target_win=target_win,
+		feature_target_intersection=intersect
+	)
+	x_vlmts, y_vlmts = next(iter(vlmts))
