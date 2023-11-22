237dc26b8669c165f056eb37d1ca232fdc8afda8
hadar <hadar933@gmail.com>
2023-11-22 16:17:08 +0200

    [encoders] added a savgol based derivative calculation

--- DataHandler/encoders.py
+++ DataHandler/encoders.py
@@ -1,12 +1,54 @@
+from typing import Optional
+
 import numpy as np
 import pandas as pd
 import torch
 from scipy import special
 from loguru import logger
+from scipy import signal
+
+
+def derivative(
+		x,
+		dt: float,
+		smooth: str = 'between',
+		window_length: Optional[int] = None,
+		polyorder: Optional[int] = None
+) -> tuple:
+	"""
+	a function that performs 1st and 2nd derivatives with possible smoothing in between
+	:param x: signal to derive
+	:param dt: time delta between x[i+1] and x[i] for all i=0,1,...,len(x)-1
+	:param smooth: one of the following
+					- between: x <smooth> x_dot <smooth> x_ddot
+					- signal: x <smooth> x_dot x_ddot
+					- none: x <smooth> x_dot x_ddot
+	:param window_length: possible window length for smoothing
+	:param polyorder: possible polyorder for smoothing
+
+	:return:
+	"""
+	if window_length is None: window_length = 51
+	if polyorder is None: polyorder = 3
+	smooth = smooth.lower()
+	smooth_options = {'between', 'signal', 'none'}
+	if smooth == 'none':
+		xdot = np.gradient(x, dt)
+		xddot = np.gradient(xdot, dt)
+	elif smooth == 'signal':
+		xdot = signal.savgol_filter(x, window_length, polyorder, deriv=1, delta=dt)
+		xddot = np.gradient(xdot, dt)
+	elif smooth == 'between':
+		xdot = signal.savgol_filter(x, window_length, polyorder, deriv=1, delta=dt)
+		xddot = signal.savgol_filter(x, window_length, polyorder, deriv=2, delta=dt)
+	else:
+		raise ValueError(f'Smoothing scheme `{smooth}` not in {smooth_options}.')
+
+	return xdot, xddot
 
 
 class Encoder:
-	def __init__(self, encoding_tasks: list[str] | None):
+	def __init__(self, encoding_tasks: list[str] | None = None):
 		self.encoding_tasks = encoding_tasks or []
 		self.task_map = {
 			method: getattr(self, method) for method in
@@ -57,33 +99,32 @@ class Encoder:
 			df: pd.DataFrame,
 			**kwargs
 	) -> pd.DataFrame:
-		dt = kwargs.get('inertial_force_dt', 1e-4)  # sec
-		wing_mass = kwargs.get('inertial_force_wing_mass', 0.003)  # kg (=3g)
+		dt = kwargs.get('inertial_force_dt', (df.index[1] - df.index[0]).total_seconds())
+		wing_mass = kwargs.get('inertial_force_wing_mass', 0.0007)  # kg (=3g)
 		if 'center_of_mass' not in df.columns:
 			df = self.center_of_mass(df, **kwargs)
 		logger.info(f'Encoding inertial force using dt={dt}, wing_mass={wing_mass}')
-		center_of_mass_z = np.vstack(df['center_of_mass'])[:, 2] / 1000  # meters
-		center_of_mass_z_dot = np.gradient(center_of_mass_z, dt)
-		center_of_mass_z_ddot = np.gradient(center_of_mass_z_dot, dt)
-		force = - wing_mass * center_of_mass_z_ddot
+		center_of_mass_y = np.vstack(df['center_of_mass'])[:, 1] / 1000  # the vertical disposition in meters
+		dot, ddot = derivative(center_of_mass_y, dt)
+		force = wing_mass * ddot
 		df['F_inertia'] = force
 		return df
 
-	def derivatives(
-			self,
-			df: pd.DataFrame,
-			**kwargs
-	) -> pd.DataFrame:
-		cols = kwargs.get('deriv_cols', self._angle_cols)
-		logger.info(f'Encoding derivative for cols = {cols}')
-		time = df.index.to_series().diff().dt.total_seconds().values[:, None]
-
-		col_dot_names = [f"{col}_dot" for col in cols]
-		df[col_dot_names] = df[cols].diff() / time
-
-		col_ddot_names = [f"{col}_ddot" for col in cols]
-		df[col_ddot_names] = df[col_dot_names].diff() / time
-		return df
+	# def derivatives(
+	# 		self,
+	# 		df: pd.DataFrame,
+	# 		**kwargs
+	# ) -> pd.DataFrame:
+	# 	cols = kwargs.get('deriv_cols', self._angle_cols)
+	# 	logger.info(f'Encoding derivative for cols = {cols}')
+	# 	time = df.index.to_series().diff().dt.total_seconds().values[:, None]
+	#
+	# 	col_dot_names = [f"{col}_dot" for col in cols]
+	# 	df[col_dot_names] = df[cols].diff() / time
+	#
+	# 	col_ddot_names = [f"{col}_ddot" for col in cols]
+	# 	df[col_ddot_names] = df[col_dot_names].diff() / time
+	# 	return df
 
 	def hermite(
 			self,
@@ -137,12 +178,20 @@ class Encoder:
 
 
 if __name__ == '__main__':
-	angles = torch.load(r"G:\My Drive\Master\Lab\Thesis\Results\10_10_2023\kinematics.pt")
-	forces = torch.load(r"G:\My Drive\Master\Lab\Thesis\Results\10_10_2023\forces.pt")
-	df = pd.DataFrame(
-		torch.concat([angles[0], forces[0]], dim=1),
-		columns=['theta', 'phi', 'psi', 'F1', 'F2', 'F3', 'F4']
-	)
-	enc = Encoder(['angle', 'torque'])
-	df1 = enc.run(df)
-	z = 1
+	from Utilities import utils
+
+	exp_date = '19_10_2023'
+	exp_name = '[F=7.886_A=M_PIdiv5.401_K=0.03]'
+	merge_name = 'merged_data.pkl'
+	merge_pp_name = 'merged_data_preprocessed_and_encoded.pkl'
+	_df = pd.read_pickle(fr"E:\Hadar\experiments\{exp_date}\results\{exp_name}\{merge_pp_name}")
+	enc = Encoder()
+	_df = _df.rename(columns={'F_inertia': 'F_inertia_old'})
+
+	df1 = enc.inertial_force(_df)
+
+	_df['com_y'] = np.vstack(_df['center_of_mass'])[:, 1] / 1000
+	_df['F1+F2+F3+F4-F_inertia'] = _df['F1+F2+F3+F4'] - _df['F_inertia']
+
+	utils.plot_df_with_plotly(df1.drop(columns=['F_inertia_old']).iloc[:2000], ignore_cols='p0 p1 p2 center_of_mass'.split())
+	z=2
