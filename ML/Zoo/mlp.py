db7b4be7ea775316ac9af412efac959fc0d0e544
hadar <hadar.urecsys@gmail.com>
2023-12-07 13:00:52 +0200

    [mlp] minor __str__ changes

--- ML/Zoo/mlp.py
+++ ML/Zoo/mlp.py
@@ -18,6 +18,7 @@ class MLP(nn.Module):
         self.history_size = history_size
         self.output_size = output_size
         self.flatten = nn.Flatten()
+        self.hidden_dims_list = hidden_dims_list
         self.input_layer = nn.Linear(input_size * history_size, hidden_dims_list[0])
         self.hidden_layers = nn.ModuleList()
         for i in range(len(hidden_dims_list) - 1):
@@ -26,7 +27,7 @@ class MLP(nn.Module):
         self.output_layer = nn.Linear(hidden_dims_list[-1], output_size)
 
     def __str__(self):
-        return f"MLP[{self.input_size * self.history_size},{self.output_size}]"
+        return f"MLP[in={self.input_size * self.history_size},hid={self.hidden_dims_list},out={self.output_size}]"
 
     def forward(self, x):
         x = self.flatten(x)
